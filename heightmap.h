a99022690edda4df119e2900db1c48b65b21b503
Kevin Moran <kevinmoran007@gmail.com>
2016-10-18 16:44:56 +1000

    Initial commit

---
+++ heightmap.h
@@ -0,0 +1,125 @@
+#pragma once
+#include <assert.h>
+
+//Save height data to file
+void write_height_pgm(const char* filename, const unsigned char* image_data, int width, int height){
+    FILE* fp = fopen(filename, "w");
+    fprintf(fp, "P2\n");
+    fprintf(fp, "%d %d \n", width, height);
+    fprintf(fp, "255\n"); //Colour resolution
+    for(int i=0; i<height; i++){
+        for(int j=0; j<width; j++){
+            fprintf(fp, "%d ", image_data[width*i +j]);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+//Read height data from file
+bool read_height_pgm(const char* filename, unsigned char** image_data, int &width, int &height){
+    FILE* fp = fopen(filename, "r");
+    if(!fp){
+        printf("Error reading %s, file does not exist\n", filename);
+        return false;
+    }
+
+    //Read header
+    {
+        char line[1024];
+        line[0] = '\0';
+        fgets(line, 1024, fp); 
+        assert(line[0]=='P' && line[1]=='2');
+        fgets(line, 1024, fp);
+        if(sscanf(line, "%d %d", &width, &height) != 2) printf("Error reading width, height from pgm file\n");
+        fgets(line, 1024, fp);
+        assert(line[0]=='2' && line[1]=='5' && line[2]=='5');
+    }
+    *image_data = (unsigned char*)malloc(width*height*sizeof(unsigned char));
+
+    int x = 0, i=0;
+	while(!feof(fp)){
+        fscanf(fp, "%d", &x);
+		(*image_data)[i] = (unsigned char)x;
+        i++;
+    }
+    fclose(fp);
+    return true;
+}
+
+//Generates a square plane of n*n vertices spanning (2*size)*(2*size) world units (centered on origin)
+void gen_vert_plane(float **verts, int &point_count, int n, float size){
+    float cell_size = 2*size/(n-1);
+    point_count = n*n;
+
+    *verts = (float*)malloc(3*point_count*sizeof(float));
+
+    int i=0;
+    float z_pos =  -size;
+    float x_pos;
+    for(int r=0; r<n; r++){
+        x_pos = -size;
+        for(int c=0; c<n; c++, i++){
+            (*verts)[3*i    ] = x_pos;
+            (*verts)[3*i + 1] = 0;
+            (*verts)[3*i + 2] = z_pos;
+            x_pos+=cell_size;
+        }
+        z_pos+=cell_size;
+    }
+}
+
+//Generates a square plane of n*n vertices spanning (2*size)*(2*size) world units (centered on origin)
+//Incorporates height from image_data array
+void gen_vert_plane(float **verts, int &point_count, const unsigned char* image_data, int n, float size){
+    float cell_size = 2*size/(n-1);
+    point_count = n*n;
+
+    *verts = (float*)malloc(3*point_count*sizeof(float));
+
+    int i=0;
+    float z_pos =  -size;
+    float x_pos;
+    for(int r=0; r<n; r++){
+        x_pos = -size;
+        for(int c=0; c<n; c++, i++){
+            (*verts)[3*i    ] = x_pos;
+            (*verts)[3*i + 1] = image_data[i];
+            (*verts)[3*i + 2] = z_pos;
+            x_pos+=cell_size;
+        }
+        z_pos+=cell_size;
+    }
+}
+
+//Writes y-components of image_data into verts and buffers data to points_vbo
+void reload_height_data (float **verts, const unsigned char* image_data, int n, GLuint points_vbo){
+    int i=0;
+    for(int r=0; r<n; r++){
+        for(int c=0; c<n; c++, i++){
+            (*verts)[3*i + 1] = image_data[i];
+        }
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, n*n*3*sizeof(float), *verts, GL_STATIC_DRAW);
+}
+
+//Generates index buffer for n*n height field
+void gen_heightmap_indices(int** indices, int &num_indices, int n){
+    int num_quads = (n-1)*(n-1);
+    num_indices = 3*2*num_quads; //3 verts per tri, 2 tris per quad
+    *indices = (int*)malloc(num_indices*sizeof(int));
+
+    int i=0;
+    for(int v=n; v<n*n; v++){ //Add two triangles per quad
+        if((v+1)%n != 0){
+            (*indices)[i++] = v;
+            (*indices)[i++] = v-n+1;
+            (*indices)[i++] = v-n;
+
+            (*indices)[i++] = v;
+            (*indices)[i++] = v+1;
+            (*indices)[i++] = v-n+1;
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+GLFWwindow* window = NULL;
+int gl_width = 360;
+int gl_height = 240;
+float gl_aspect_ratio = (float)gl_width/gl_height;
+
+#include "init_gl.h"
+#include "maths_funcs.h"
+#include "Shader.h"
+#include "FlyCam.h"
+#include "heightmap.h"
+
+int main(){
+	if (!init_gl(window, gl_width, gl_height)){ return 1; }
+
+    int heightmap_n = 64;
+    int width=heightmap_n, height=heightmap_n;
+    unsigned char* image_data;
+
+    if(!read_height_pgm("terrain.pgm", &image_data, width, height)){
+        width = 64, height = 64;
+        image_data = (unsigned char*)malloc(width*height*sizeof(unsigned char));
+        memset(image_data, 0, width*height);
+    }
+
+    //TODO call this when done editing heightmap
+    // free(image_data);
+
+    float* vp;
+    int* indices;
+    int point_count, num_indices;
+
+    gen_vert_plane(&vp, point_count, image_data, heightmap_n, 10.0f);
+    gen_heightmap_indices(&indices, num_indices, heightmap_n);
+
+	GLuint points_vbo;
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, point_count*3*sizeof(float), vp, GL_STATIC_DRAW);
+
+    GLuint index_vbo;
+    glGenBuffers(1, &index_vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_vbo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, num_indices*sizeof(int), indices, GL_STATIC_DRAW);
+
+    GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	//free(vp); //TODO should probably free this at some stage
+    free(indices);
+
+    //Load shader
+	Shader pass_shader("MVP.vert", "uniform_colour.frag");
+	GLuint colour_loc = glGetUniformLocation(pass_shader.prog_id, "colour");
+	glUseProgram(pass_shader.prog_id);
+
+    FlyCam fly_cam;
+    fly_cam.init(vec3(0,12,15), vec3(0,0,0));
+    glUniformMatrix4fv(pass_shader.P_loc, 1, GL_FALSE, fly_cam.P.m);
+    glUniform4fv(colour_loc, 1, vec4(0.2f,0.4f,0.1f,1).v);
+    glUniformMatrix4fv(pass_shader.M_loc, 1, GL_FALSE, identity_mat4().m);
+
+    double curr_time = glfwGetTime(), prev_time, dt;
+	//-------------------------------------------------------------------------------------//
+	//-------------------------------------MAIN LOOP---------------------------------------//
+	//-------------------------------------------------------------------------------------//
+	while (!glfwWindowShouldClose(window)) {
+		//Get dt
+		prev_time = curr_time;
+		curr_time = glfwGetTime();
+		dt = curr_time - prev_time;
+		if (dt > 0.1) dt = 0.1;
+
+		//Get Input
+		glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			continue;
+		}
+		static bool fly_cam_enabled = true;
+		static bool F_was_pressed = false;
+		if (glfwGetKey(window, GLFW_KEY_F)) {
+			if(!F_was_pressed) {
+				fly_cam_enabled = !fly_cam_enabled;
+				F_was_pressed = true;
+			}
+		}
+		else F_was_pressed = false;
+
+        if (glfwGetKey(window, GLFW_KEY_R)) {
+            reload_height_data(&vp, image_data, heightmap_n, points_vbo);
+        }
+
+        //Save updated image_data to file
+        if (glfwGetKey(window, GLFW_KEY_H)) {
+            write_height_pgm("terrain.pgm", image_data, width, height);
+        }
+
+        double mouse_xpos, mouse_ypos;
+        glfwGetCursorPos(window, &mouse_xpos, &mouse_ypos);
+        if(mouse_xpos>0 && mouse_xpos<gl_width && mouse_ypos>0 && mouse_ypos<gl_height) {
+            //Mouse is within viewport
+
+        }
+
+		if(fly_cam_enabled) fly_cam.update(dt);
+		else{
+			// if(g_input[MOVE_FORWARD]) {
+			// }
+			// if(g_input[MOVE_LEFT]) {
+			// }
+			// if(g_input[MOVE_BACK]) {		
+			// }
+			// if(g_input[MOVE_RIGHT]) {		
+			// }
+		}
+
+		//Rendering
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(pass_shader.prog_id);
+		glBindVertexArray(vao);
+		glUniformMatrix4fv(pass_shader.V_loc, 1, GL_FALSE, fly_cam.V.m);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_vbo);
+        glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+	}//end main loop
+
+    return 0;
+}
